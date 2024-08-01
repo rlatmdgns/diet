14b30f125917bdc52f501da6d4c27afd3b53622a
김승훈 <rlatmdgns94@naver.com>
2024-08-01 21:05:05 +0900

    feat: 달력 추가

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from 'react'
+import Link from 'next/link'
 
 const signPage = () => {
   return (
     <div>
       <h1>Sign In Page</h1>
+      <Link href="http://api.rumor-lab.com/oauth2/authorization/google">로그인</Link>
     </div>
-  );
-};
+  )
+}
 
-export default signPage;
+export default signPage
