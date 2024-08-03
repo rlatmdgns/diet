bed7e44699f974f75d415ab5f21fd7de8d376783
김승훈 <rlatmdgns94@naver.com>
2024-08-03 21:21:46 +0900

    test: api

--- src/shared/lib/http.tsx
+++ src/shared/lib/http.tsx
@@ -1,13 +1,10 @@
-import Cookies from 'js-cookie'
 import returnFetch from 'return-fetch'
 
 const baseUrl = process.env.NEXT_PUBLIC_DEV_HOST
-const token = Cookies.get('Authorization')
 
 export const fetchExtended = returnFetch({
   baseUrl,
   headers: {
     Accept: 'application/json',
-    authorization: `Bearer ${token}`,
   },
 })
